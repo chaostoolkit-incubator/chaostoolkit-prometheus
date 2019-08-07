6782e329f917bd64523c410e74f19ab725b4bc8a
sneJ- <jens.rowekamp@mariadb.com>
2019-08-07 14:35:46 +0200

    DBAAS-977 - first actual logic for query_results_lower_than_threshold
    Signed-off-by: sneJ- <jens.rowekamp@mariadb.com>

--- chaosprometheus/verification/probes.py
+++ chaosprometheus/verification/probes.py
@@ -11,8 +11,29 @@ def query_results_lower_than_threshold(threshold: int,
     given threshold. If so returns True, otherwise False.
     If no threshold is given it throws an exception.
     """
+    rtn = True
     if threshold is None:
         raise Exception("No threshold given")
-    logger.info("threshold: %d" % (threshold,))
-    print(value)
-    return True
+
+    logger.debug(str(value))
+
+    # check if we got none, a result (query) or multiple results (range_query)
+    # from Prometheus. If no results are provided, throw a warning and return
+    # with True / ok
+
+    # handle Prometheus range_query
+
+    # handle Prometheus query
+    for entry in value.data.result:
+        if entry.value[1] < threshold:
+            logger.debug("Probe: value %f is below threshold %d" %
+                         (entry.value[1], threshold))
+        else:
+            logger.error("Probe: value %f is higher than threshold %d" %
+                         (entry.value[1], threshold))
+            rtn = False
+
+    if rtn:
+        logger.info("Probe: ok, all values are below the given threshold")
+
+    return rtn
