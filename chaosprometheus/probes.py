c559f8a7fb909037a0616208d4158de14441c8e1
Andy Kuszyk <andy.kuszyk@form3.tech>
2020-06-03 16:46:38 +0100

    Drafted query value probe

--- chaosprometheus/probes.py
+++ chaosprometheus/probes.py
@@ -47,6 +47,17 @@ def query(query: str, when: str = None, timeout: float = None,
     return r.json()
 
 
+def query_value(query_string: str, when: str = None, timeout: float = None,
+          configuration: Configuration = None,
+          secrets: Secrets = None):
+    results = query(query_string, when, timeout, configuration, secrets)
+    if len(results['data']['result']) != 1:
+        raise FailedActivity("Expected a Prometheus response with just one result")
+    if len(results['data']['result'][0]['value']) != 2:
+        raise FailedActivity("Expected a Prometheus result with just one value")
+    return float(results['data']['result'][0]['value'][1])
+
+
 def query_interval(query: str, start: str, end: str, step: int = 1,
                    timeout: float = None, configuration: Configuration = None,
                    secrets: Secrets = None) -> Dict[str, Any]:
